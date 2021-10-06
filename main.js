fa28eb3d9c4f2a176e7c69f7a82671a154748679
Vladimir Stojanovic <st.vladas@hotmail.com>
2021-10-06 16:16:32 +0200

    date formated

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const inputValue = document.querySelector('#search');
 const searchButton = document.querySelector('.searchButton');
 const imageContainer = document.querySelector('.image');
+const dateContainer = document.querySelector('.joined-date');
 const nameContainer = document.querySelector('.main_profile-name');
 const unContainer = document.querySelector('.main_profile-username');
 const bioContainer = document.querySelector('.bio-info');
@@ -17,7 +18,7 @@ const noresults = document.querySelector('.noresults');
 const client_secret = '7753fdad58bb42c0d28741064dc7aa67381d8530';
 const client_id = 'Iv1.71a7a9f9a18f113a';
 
-const fetchUsers = async (user) => {
+const fetchUsers = async (user = 'octocat') => {
     const api_call = await fetch(
         `https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`
     );
@@ -26,65 +27,78 @@ const fetchUsers = async (user) => {
     return { data };
 };
 
+const updateUsers = (res) => {
+    function setOPacity(api_data, data) {
+        if (api_data === null) {
+            data.style.opacity = 0.5;
+            data.previousElementSibling.style.opacity = 0.5;
+            return 'Not available';
+        } else {
+            return `${api_data}`;
+        }
+    }
+
+    if (res.data.message !== 'Not Found') {
+        noresults.style.display = 'none';
+
+        imageContainer.innerHTML = `<img src="${res.data.avatar_url}"/>`;
+
+        nameContainer.innerHTML =
+            res.data.name == null ? `${res.data.login}` : `${res.data.name}`;
+
+        unContainer.innerHTML = `@${res.data.login}`;
+
+        var parts = res.data.created_at.split('T').shift().split('-');
+        var myDate = new Date(parts[0], parts[1] - 1, parts[2]);
+
+        dateContainer.innerHTML = `Joined ${myDate
+            .toDateString()
+            .split(' ')
+            .slice(1)
+            .join(' ')}`;
+
+        if (res.data.bio == null) {
+            bioContainer.innerHTML = 'This profile has no bio';
+            bioContainer.style.opacity = 0.75;
+        } else {
+            bioContainer.innerHTML = `${res.data.bio}`;
+        }
+
+        reposContiner.innerHTML = `${res.data.public_repos}`;
+
+        followersContiner.innerHTML = `${res.data.followers}`;
+
+        followingContiner.innerHTML = `${res.data.following}`;
+
+        cityContainer.innerHTML = setOPacity(res.data.location, cityContainer);
+
+        urlContainer.innerHTML = setOPacity(res.data.html_url, urlContainer);
+
+        twitterContainer.innerHTML = setOPacity(
+            res.data.twitter_username,
+            twitterContainer
+        );
+
+        companyContainer.innerHTML = setOPacity(
+            res.data.company,
+            companyContainer
+        );
+    } else {
+        noresults.style.display = 'block';
+    }
+};
+
 const showData = () => {
     fetchUsers(inputValue.value).then((res) => {
         console.log(res);
 
-        function setOPacity(api_data, data) {
-            if (api_data === null) {
-                data.style.opacity = 0.5;
-                data.previousElementSibling.style.opacity = 0.5;
-                return 'Not available';
-            } else {
-                return `${api_data}`;
-            }
-        }
+        updateUsers(res);
+    });
+};
 
-        if (res.data.message !== 'Not Found') {
-            noresults.style.display = 'none';
-
-            imageContainer.innerHTML = `<img src="${res.data.avatar_url}"/>`;
-
-            nameContainer.innerHTML =
-                res.data.name == null
-                    ? `${res.data.login}`
-                    : `${res.data.name}`;
-
-            unContainer.innerHTML = `@${res.data.login}`;
-
-            bioContainer.innerHTML =
-                res.data.bio == null
-                    ? 'This profile has no bio'
-                    : `${res.data.bio}`;
-
-            reposContiner.innerHTML = `${res.data.public_repos}`;
-
-            followersContiner.innerHTML = `${res.data.followers}`;
-
-            followingContiner.innerHTML = `${res.data.following}`;
-
-            cityContainer.innerHTML = setOPacity(
-                res.data.location,
-                cityContainer
-            );
-
-            urlContainer.innerHTML = setOPacity(
-                res.data.html_url,
-                urlContainer
-            );
-
-            twitterContainer.innerHTML = setOPacity(
-                res.data.twitter_username,
-                twitterContainer
-            );
-
-            companyContainer.innerHTML = setOPacity(
-                res.data.company,
-                companyContainer
-            );
-        } else {
-            noresults.style.display = 'block';
-        }
+showDefaultData = () => {
+    fetchUsers().then((res) => {
+        updateUsers(res);
     });
 };
 
@@ -92,3 +106,7 @@ searchButton.addEventListener('click', (e) => {
     e.preventDefault();
     showData();
 });
+
+window.onload = () => {
+    showDefaultData();
+};
